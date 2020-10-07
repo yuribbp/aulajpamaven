91733b0f4bf76fa256afc854a231b9546a5996bf
Yuri Baldan <yuribbp@gmail.com>
2020-10-07 17:46:13 -0300

    Find by Id implementation

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -10,18 +10,23 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		System.out.println("*-------------------------*");
+		System.out.println("*          Start          *");
+		System.out.println("*-------------------------*");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
 		
-		System.out.println("Done!");
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		
+		System.out.println();
+		System.out.println("*-------------------------*");
+		System.out.println("*          Done!          *");
+		System.out.println("*-------------------------*");
+		
+		em.close();
+		emf.close();
 	}
 }
